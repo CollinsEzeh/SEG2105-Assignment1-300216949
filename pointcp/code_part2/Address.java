baafe451e94eae3f0785690d61680f0853788315
Melgo103 <melgo103@uottawa.ca>
2022-10-08 16:13:42 -0400

    cleanup

--- pointcp/code_part2/Address.java
+++ pointcp/code_part2/Address.java
@@ -1,13 +1,27 @@
 package code_part2;
 
 public class Address {
-    public String street;
-    public int number;
-    public String postal;
-    public Address (String street, int number, String postal ){
-        this.street= street;
-        this.number= number;
-        this.postal= postal;
+
+    private String street;
+    private int number;
+    private String postal;
+
+    public Address(String street, int number, String postal) {
+        this.street = street;
+        this.number = number;
+        this.postal = postal;
+    }
+
+    public String getStreet() {
+        return this.street;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public String getPostal() {
+        return this.postal;
     }
 
 }
--- pointcp/code_part2/Employee.java
+++ pointcp/code_part2/Employee.java
@@ -1,16 +1,35 @@
 package code_part2;
 
 public class Employee {
-    public String name;
-    public int hours;
-    public double rate;
-    public Address address;
-    public Employee(String name, int hours,double rate, Address address ){
-        this.name= name;
+
+    private String name;
+    private int hours;
+    private double rate;
+    private Address address;
+
+    public Employee(String name, int hours, double rate, Address address) {
+        this.name = name;
         this.hours = hours;
-        this.rate= rate;
+        this.rate = rate;
         this.address = address;
-        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHours() {
+        return this.hours;
+    }
+
+    public double getRate() {
+        return this.rate;
+    }
+
+    public Address getAddress() {
+        return this.address;
+    }
+
 }
 
 
